09d222dd054400eef639779e1fc1b3fdccde8010
catherine <h1codingqueenh1@gmail.com>
2022-12-31 11:38:37 -0500

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by adobada on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
